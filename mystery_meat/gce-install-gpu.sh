7dc79e273aead494af087d62796f4f31e9325deb
Melanie Warrick <warrick@google.com>
2017-07-17 17:52:04 -0700

    Removed data load

--- mystery_meat/gce-install-gpu.sh
+++ mystery_meat/gce-install-gpu.sh
@@ -125,13 +125,3 @@ mkdir code && cd code
 	
 echo
 echo "Setup complete"
-
-# Loading the SVHN data
-#
-# mkdir data && cd data
-# wget http://ufldl.stanford.edu/housenumbers/train.tar.gz
-# wget http://ufldl.stanford.edu/housenumbers/test.tar.gz
-# wget http://ufldl.stanford.edu/housenumbers/extra.tar.gz
-# tar -xvzf test.tar.gz && tar -xvzf train.tar.gz && tar -xvzf extra.tar.gz
-# rm *.tar.gz
-# cd ../SVHNClassifier-PyTorch/ && python convert_to_lmdb.py --data_dir ../data
